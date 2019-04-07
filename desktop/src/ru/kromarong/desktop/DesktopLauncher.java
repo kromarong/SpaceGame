4b0104f3dd816b87297e0ed2cb72f470866efa36
kromarong <kromarong@gmail.com>
2019-04-07 12:34:44 +0300

    ..

---
+++ desktop/src/ru/kromarong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.kromarong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.kromarong.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
