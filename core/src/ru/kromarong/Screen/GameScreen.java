603ac10d95310732b5b3ab324b31c51d46e6f3d5
kromarong <kromarong@gmail.com>
2019-04-17 16:39:33 +0300

    hw4

--- core/src/ru/kromarong/Screen/GameScreen.java
+++ core/src/ru/kromarong/Screen/GameScreen.java
@@ -1,141 +1,85 @@
-package ru.kromarong.Screen;
+package ru.kromarong.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.kromarong.Base.BaseScreen;
+
+import ru.kromarong.base.BaseScreen;
+import ru.kromarong.math.Rect;
+import ru.kromarong.sprite.Background;
+import ru.kromarong.sprite.Ship;
+import ru.kromarong.sprite.Star;
 
 public class GameScreen extends BaseScreen {
 
-    private Vector2 touch;
-    private Vector2 objPos;
-    private Vector2 speed;
-    private Vector2 targetPos;
-    private Texture img;
-    private Texture donut;
-    private boolean leftMove = false;
-    private boolean rightMove = false;
-    private boolean isExistDonut = false;
-    private boolean isUpPressed;
-    private boolean isDownPressed;
-    private boolean isLeftPressed;
-    private boolean isRightPressed;
+    private Ship ship;
+    private Texture bg;
+    private Texture shipImg;
+    private Background background;
+    private TextureAtlas atlas;
+    private Star starList[];
 
     @Override
     public void show() {
         super.show();
-        touch = new Vector2();
-        objPos = new Vector2(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
-        targetPos = new Vector2();
-        speed = new Vector2(0.0f, 0.0f);
-        img = new Texture("rightMiku.png");
-        donut = new Texture("donut.png");
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        shipImg = new Texture("textures/ship.png");
+        ship = new Ship(new TextureRegion(shipImg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        starList = new Star[256];
+        for (int i = 0; i < starList.length; i++) {
+            starList[i] = new Star(atlas);
+        }
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : starList) {
+            star.resize(worldBounds);
+        }
+        ship.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        if ((speed.x > 0) && (!rightMove)) {
-            img = new Texture("rightMiku.png");
-            rightMove = true;
-            leftMove = false;
-        } else if ((speed.x < 0) && (!leftMove)) {
-            img = new Texture("leftMiku.png");
-            leftMove = true;
-            rightMove = false;
+        update(delta);
+        draw();
+    }
+
+    private void update(float delta) {
+        for (Star star : starList) {
+            star.update(delta);
         }
-        objPos.add(speed);
+        ship.update(delta);
+    }
+
+    private void draw() {
         batch.begin();
-        batch.draw(img, objPos.x, objPos.y);
-        if (isExistDonut) {
-            batch.draw(donut, targetPos.x, targetPos.y);
+        background.draw(batch);
+        for (Star star : starList) {
+            star.draw(batch);
         }
+        ship.draw(batch);
         batch.end();
-        if ((objPos.y > Gdx.graphics.getHeight() - 65) || (objPos.y < -66) ||
-                (objPos.x > Gdx.graphics.getWidth() - 65) || (objPos.x < -66)) {
-            speed.setZero();
-        }
-        if (isExistDonut && (Math.abs(objPos.x - targetPos.x) < 32) && ((Math.abs(objPos.y - targetPos.y) < 32))) {
-            speed.setZero();
-            img = new Texture("happyMiku.png");
-            isExistDonut = false;
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
-        donut.dispose();
+        bg.dispose();
+        shipImg.dispose();
+        atlas.dispose();
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        switch (keycode) {
-            case (19): //up
-                speed.set(speed.x, 1);
-                isUpPressed = true;
-                break;
-            case (20): //down
-                speed.set(speed.x, -1);
-                isDownPressed = true;
-                break;
-            case (21): //left
-                speed.set(-1, speed.y);
-                isLeftPressed = true;
-                break;
-            case (22): //right
-                speed.set(1, speed.y);
-                isRightPressed = true;
-                break;
-        }
-        return super.keyDown(keycode);
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        switch (keycode) {
-            case (19): //up
-                isUpPressed = false;
-                if (isDownPressed) {
-                    break;
-                }
-                speed.set(speed.x, 0);
-                break;
-            case (20): //down
-                isDownPressed = false;
-                if (isUpPressed) {
-                    break;
-                }
-                speed.set(speed.x, 0);
-                break;
-            case (21): //left
-                isLeftPressed = false;
-                if (isRightPressed) {
-                    break;
-                }
-                speed.set(0, speed.y);
-                break;
-            case (22): //right
-                isRightPressed = false;
-                if (isLeftPressed) {
-                    break;
-                }
-                speed.set(0, speed.y);
-                break;
-        }
-        return super.keyUp(keycode);
-
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        targetPos.set(touch.x - 32, touch.y - 32);
-        isExistDonut = true;
-        speed = touch.sub(objPos);
-        speed.nor();
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer) {
+        ship.touchDown(touch,pointer);
+        return super.touchDown(touch, pointer);
     }
 }
