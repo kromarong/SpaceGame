7e684552b7fb923793c42fe67ab01e084f8a70be
kromarong <kromarong@gmail.com>
2019-06-18 13:16:29 +0300

    Rework shield logic and shieldbar&healthbar textures

--- core/src/ru/kromarong/sprite/HealthBar.java
+++ core/src/ru/kromarong/sprite/HealthBar.java
@@ -1,127 +1,97 @@
 package ru.kromarong.sprite;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.kromarong.base.Sprite;
-import ru.kromarong.math.Rect;
 
 public class HealthBar extends Sprite {
 
     private MainShip mainShip;
 
-    public HealthBar(TextureRegion region, MainShip mainShip, Rect worldBounds) {
-        super(region, 33,1,33);
+    public HealthBar(TextureAtlas atlas, MainShip mainShip) {
+        this.regions = initRegions(atlas);
         this.mainShip = mainShip;
-        setHeightProportion(0.05f);
-        setLeft( -getWidth() * 1.4f);
+        setHeightProportion(0.04f);
+        setLeft( -getWidth() * 1.3f);
         setBottom(-0.5f);
+    }
+
+    private TextureRegion[] initRegions(TextureAtlas atlas){
+        TextureRegion[] regions = new TextureRegion[11];
+        int count = 0;
+        while(count <= 10){
+            regions[count] = findRegions(count, atlas);
+            count++;
+        }
+        return regions;
+    }
+
+    private TextureRegion findRegions(int count, TextureAtlas atlas) {
+        switch (count){
+            case 0:
+                return atlas.findRegion("health0");
+            case 1:
+                return atlas.findRegion("health1");
+            case 2:
+                return atlas.findRegion("health2");
+            case 3:
+                return atlas.findRegion("health3");
+            case 4:
+                return atlas.findRegion("health4");
+            case 5:
+                return atlas.findRegion("health5");
+            case 6:
+                return atlas.findRegion("health6");
+            case 7:
+                return atlas.findRegion("health7");
+            case 8:
+                return atlas.findRegion("health8");
+            case 9:
+                return atlas.findRegion("health9");
+            case 10:
+                return atlas.findRegion("health10");
+        }
+
+        return atlas.findRegion("health10");
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
         switch (mainShip.getHp()){
-            case 33:
-                frame = 0;
-                break;
-            case 32:
-                frame = 1;
-                break;
-            case 31:
-                frame = 2;
-                break;
-            case 30:
-                frame = 3;
-                break;
-            case 29:
-                frame = 4;
-                break;
-            case 28:
-                frame = 5;
-                break;
-            case 27:
-                frame = 6;
-                break;
-            case 26:
-                frame = 7;
-                break;
-            case 25:
-                frame = 8;
-                break;
-            case 24:
-                frame = 9;
-                break;
-            case 23:
+            case 10:
                 frame = 10;
                 break;
-            case 22:
-                frame = 11;
-                break;
-            case 21:
-                frame = 12;
-                break;
-            case 20:
-                frame = 13;
-                break;
-            case 19:
-                frame = 14;
-                break;
-            case 18:
-                frame = 15;
-                break;
-            case 17:
-                frame = 16;
-                break;
-            case 16:
-                frame = 17;
-                break;
-            case 15:
-                frame = 18;
-                break;
-            case 14:
-                frame = 19;
-                break;
-            case 13:
-                frame = 20;
-                break;
-            case 12:
-                frame = 21;
-                break;
-            case 11:
-                frame = 22;
-                break;
-            case 10:
-                frame = 23;
-                break;
             case 9:
-                frame = 24;
+                frame = 9;
                 break;
             case 8:
-                frame = 25;
+                frame = 8;
                 break;
             case 7:
-                frame = 26;
+                frame = 7;
                 break;
             case 6:
-                frame = 27;
+                frame = 6;
                 break;
             case 5:
-                frame = 28;
+                frame = 5;
                 break;
             case 4:
-                frame = 29;
+                frame = 4;
                 break;
             case 3:
-                frame = 30;
+                frame = 3;
                 break;
             case 2:
-                frame = 31;
+                frame = 2;
                 break;
             case 1:
-                frame = 32;
+                frame = 1;
                 break;
             case 0:
-                frame = 33;
+                frame = 0;
                 break;
         }
     }
--- core/src/ru/kromarong/sprite/ShieldBar.java
+++ core/src/ru/kromarong/sprite/ShieldBar.java
@@ -1,58 +1,72 @@
 package ru.kromarong.sprite;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.kromarong.base.Sprite;
-import ru.kromarong.math.Rect;
 
 public class ShieldBar extends Sprite {
     private Shield shield;
 
-    public ShieldBar(TextureRegion region, Shield shield, Rect worldBounds) {
-        super(region, 11,1,11);
+    public ShieldBar(TextureAtlas atlas, Shield shield) {
+        this.regions = initRegions(atlas);
         this.shield = shield;
-        setHeightProportion(0.05f);
-        setRight( getWidth() * 1.5f);
+        setHeightProportion(0.04f);
+        setRight( getWidth() * 1.4f);
         setBottom(-0.5f);
+    }
+
+    private TextureRegion[] initRegions(TextureAtlas atlas){
+        TextureRegion[] regions = new TextureRegion[6];
+        int count = 0;
+        while(count <= 5){
+            regions[count] = findRegions(count, atlas);
+            count++;
+        }
+        return regions;
+    }
+
+    private TextureRegion findRegions(int count, TextureAtlas atlas) {
+
+        switch (count){
+            case 0:
+                return atlas.findRegion("shield0");
+            case 1:
+                return atlas.findRegion("shield1");
+            case 2:
+                return atlas.findRegion("shield2");
+            case 3:
+                return atlas.findRegion("shield3");
+            case 4:
+                return atlas.findRegion("shield4");
+            case 5:
+                return atlas.findRegion("shield5");
+        }
+
+        return atlas.findRegion("shield0");
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
         switch (shield.getHp()){
-
-            case 10:
-                frame = 0;
-                break;
-            case 9:
-                frame = 1;
-                break;
-            case 8:
-                frame = 2;
-                break;
-            case 7:
-                frame = 3;
-                break;
-            case 6:
-                frame = 4;
-                break;
             case 5:
                 frame = 5;
                 break;
             case 4:
-                frame = 6;
+                frame = 4;
                 break;
             case 3:
-                frame = 7;
+                frame = 3;
                 break;
             case 2:
-                frame = 8;
+                frame = 2;
                 break;
             case 1:
-                frame = 9;
+                frame = 1;
                 break;
             case 0:
-                frame = 10;
+                frame = 0;
                 break;
         }
     }
